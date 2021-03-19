55e5c20b0a6ed1123967b94bc52a01949fc10f86
faez bin mohd nasir <faeznasir97@gmail.com>
2021-03-19 16:54:35 +0800

    third commit location

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -6,6 +6,7 @@ public class Location {
 	//attribute declaration
 	@SuppressWarnings("unused")
 	private int locate;
+	private int locator;
 	private int location;
 	
 	public void setLocation(int location) {
