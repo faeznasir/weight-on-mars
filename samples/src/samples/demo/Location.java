0f98d877df10e943a18d9eca1826a035bfa43e4b
faez bin mohd nasir <faeznasir97@gmail.com>
2021-03-19 16:48:55 +0800

    commit location

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,19 @@
+package samples.demo;
+
+public class Location {
+
+	
+	//attribute declaration
+	@SuppressWarnings("unused")
+	private int location;
+	
+	public void setLocation(int location) {
+		
+		this.location = location;
+	}
+	
+	public int getLocation(int location) {
+		
+		return location;
+	}
+}
