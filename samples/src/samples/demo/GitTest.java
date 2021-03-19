320cc50525c012de057d8e9d9d56233e67f681c0
faez bin mohd nasir <faeznasir97@gmail.com>
2021-03-19 16:47:59 +0800

    Second commit

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -2,9 +2,18 @@ package samples.demo;
 
 public class GitTest {
 
+	@SuppressWarnings("unused")
+	private String gigi;
+	
+	public void Gigi() {
+		
+		gigi = "git is fun";
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	
 	}
 
 }
